2372c23b823f5967beb03b5762258caff3feb6f9
zhuk76787 <zhuk76787@gmail.com>
2024-11-18 23:47:54 +0400

    добавил цвета в расширение для UIColor

---
+++ ToDoList/ExtentionUIColor.swift
@@ -0,0 +1,17 @@
+//
+//  ExtentionForUIColor.swift
+//  ToDoList
+//
+//  Created by Дмитрий Жуков on 11/18/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var customBlack: UIColor { #colorLiteral(red: 0.01266666781, green: 0.01266666595, blue: 0.01266666781, alpha: 1) }
+    static var customWhite: UIColor { #colorLiteral(red: 0.965680182, green: 0.965680182, blue: 0.965680182, alpha: 1) }
+    static var customYellow: UIColor { #colorLiteral(red: 0.9993364215, green: 0.8632268906, blue: 0, alpha: 1) }
+    static var stroke: UIColor { #colorLiteral(red: 0.3739236593, green: 0.4084571302, blue: 0.4442664981, alpha: 1) }
+    static var customGray:UIColor { #colorLiteral(red: 0.2030287385, green: 0.2033452094, blue: 0.2129075825, alpha: 1) }
+    static var customRed:UIColor { #colorLiteral(red: 0.882973671, green: 0.1264794171, blue: 0.09929607064, alpha: 1) }
+}
