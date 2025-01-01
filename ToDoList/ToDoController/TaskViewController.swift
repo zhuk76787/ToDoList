46310f6a6d1041d144c8c43255def7f2c61a96d4
zhuk76787 <zhuk76787@gmail.com>
2025-01-01 20:05:08 +0400

    Создал TaskViewController

---
+++ ToDoList/ToDoController/TaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskViewController.swift
+//  ToDoList
+//
+//  Created by Дмитрий Жуков on 1/1/25.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
